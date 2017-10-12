98b67c4377f68b289988be36b273d375c1510c60
SRoderick <SRoderick@PC38025.uk.rebushr.com>
2017-10-12 14:29:39 +0100

    enhancements

--- src/api/Frame.java
+++ src/api/Frame.java
@@ -18,155 +18,331 @@ package api;
  */
 public class Frame {
    
-   public Frame(String distance, String time, String lat, String l, String rSSI, String lSNR, String datarate,
-         String gateway_EUI, String counter) {
-      super();
-      Distance = distance;
-      Time = time;
-      Lat = lat;
-      Long = l;
-      RSSI = rSSI;
-      LSNR = lSNR;
-      Datarate = datarate;
-      Gateway_EUI = gateway_EUI;
-      Counter = counter;
-   }
+	private String gw_lat;
+	private String gw_lon;
+	
+	
+	public String getGw_lat() {
+		return gw_lat;
+	}
+	public void setGw_lat(String gw_lat) {
+		this.gw_lat = gw_lat;
+	}
+	public String getGw_lon() {
+		return gw_lon;
+	}
+	public void setGw_lon(String gw_lon) {
+		this.gw_lon = gw_lon;
+	}
+	
+	
+	
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNet_type() {
+		return net_type;
+	}
+	public void setNet_type(String net_type) {
+		this.net_type = net_type;
+	}
+	public Frame(String time, String id, String net_type, String gw_lat, String gw_lon, String gw_alt,
+			String gw_ant_type, String gw_location, String gw_comments, String node_lat, String node_long,
+			String node_alt, String node_type, String node_location, String node_comments, String distance, String rssi,
+			String lsnr, String datr, String counter_up, String gw_addr, String dev_addr, String port, String stat,
+			String gw_band, String server_time, String modu, String chan, String gateway_time, String tmst, String codr,
+			String rfch, String freq, String size, String dev_eui, String rx_time, String payload, String endoffile) {
+		super();
+		this.time = time;
+		this.id = id;
+		this.net_type = net_type;
+		this.gw_lat = gw_lat;
+		this.gw_lon = gw_lon;
+		this.gw_alt = gw_alt;
+		this.gw_ant_type = gw_ant_type;
+		this.gw_location = gw_location;
+		this.gw_comments = gw_comments;
+		this.node_lat = node_lat;
+		this.node_long = node_long;
+		this.node_alt = node_alt;
+		this.node_type = node_type;
+		this.node_location = node_location;
+		this.node_comments = node_comments;
+		this.distance = distance;
+		this.rssi = rssi;
+		this.lsnr = lsnr;
+		this.datr = datr;
+		this.counter_up = counter_up;
+		this.gw_addr = gw_addr;
+		this.dev_addr = dev_addr;
+		this.port = port;
+		this.stat = stat;
+		this.gw_band = gw_band;
+		this.server_time = server_time;
+		this.modu = modu;
+		this.chan = chan;
+		this.gateway_time = gateway_time;
+		this.tmst = tmst;
+		this.codr = codr;
+		this.rfch = rfch;
+		this.freq = freq;
+		this.size = size;
+		this.dev_eui = dev_eui;
+		this.rx_time = rx_time;
+		this.payload = payload;
+		this.endoffile = endoffile;
+	}
+	
 
-   String Distance;
-   String Time;
-   String Lat;
-   String Long;
-   String RSSI;
-   String LSNR;
-   String Datarate;
-   String Gateway_EUI;
-   String Counter;
-   /**
-    * @return the distance
-    */
-   public String getDistance() {
-      return Distance;
-   }
-   
-   /**
-    * @param distance the distance to set
-    */
-   public void setDistance(String distance) {
-      Distance = distance;
-   }
-   
-   /**
-    * @return the time
-    */
-   public String getTime() {
-      return Time;
-   }
-   
-   /**
-    * @param time the time to set
-    */
-   public void setTime(String time) {
-      Time = time;
-   }
-   
-   /**
-    * @return the lat
-    */
-   public String getLat() {
-      return Lat;
-   }
-   
-   /**
-    * @param lat the lat to set
-    */
-   public void setLat(String lat) {
-      Lat = lat;
-   }
-   
-   /**
-    * @return the long
-    */
-   public String getLong() {
-      return Long;
-   }
-   
-   /**
-    * @param l the long to set
-    */
-   public void setLong(String l) {
-      Long = l;
-   }
-   
-   /**
-    * @return the rSSI
-    */
-   public String getRSSI() {
-      return RSSI;
-   }
-   
-   /**
-    * @param rSSI the rSSI to set
-    */
-   public void setRSSI(String rSSI) {
-      RSSI = rSSI;
-   }
-   
-   /**
-    * @return the lSNR
-    */
-   public String getLSNR() {
-      return LSNR;
-   }
-   
-   /**
-    * @param lSNR the lSNR to set
-    */
-   public void setLSNR(String lSNR) {
-      LSNR = lSNR;
-   }
-   
-   /**
-    * @return the datarate
-    */
-   public String getDatarate() {
-      return Datarate;
-   }
-   
-   /**
-    * @param datarate the datarate to set
-    */
-   public void setDatarate(String datarate) {
-      Datarate = datarate;
-   }
-   
-   /**
-    * @return the gateway_EUI
-    */
-   public String getGateway_EUI() {
-      return Gateway_EUI;
-   }
-   
-   /**
-    * @param gateway_EUI the gateway_EUI to set
-    */
-   public void setGateway_EUI(String gateway_EUI) {
-      Gateway_EUI = gateway_EUI;
-   }
-   
-   /**
-    * @return the counter
-    */
-   public String getCounter() {
-      return Counter;
-   }
-   
-   /**
-    * @param counter the counter to set
-    */
-   public void setCounter(String counter) {
-      Counter = counter;
-   }
+	
+	
+	
+	public String getGw_alt() {
+		return gw_alt;
+	}
+	public void setGw_alt(String gw_alt) {
+		this.gw_alt = gw_alt;
+	}
+	public String getGw_ant_type() {
+		return gw_ant_type;
+	}
+	public void setGw_ant_type(String gw_ant_type) {
+		this.gw_ant_type = gw_ant_type;
+	}
+	public String getGw_location() {
+		return gw_location;
+	}
+	public void setGw_location(String gw_location) {
+		this.gw_location = gw_location;
+	}
+	public String getGw_comments() {
+		return gw_comments;
+	}
+	public void setGw_comments(String gw_comments) {
+		this.gw_comments = gw_comments;
+	}
+	public String getNode_lat() {
+		return node_lat;
+	}
+	public void setNode_lat(String node_lat) {
+		this.node_lat = node_lat;
+	}
+	public String getNode_long() {
+		return node_long;
+	}
+	public void setNode_long(String node_long) {
+		this.node_long = node_long;
+	}
+	public String getNode_alt() {
+		return node_alt;
+	}
+	public void setNode_alt(String node_alt) {
+		this.node_alt = node_alt;
+	}
+	public String getNode_type() {
+		return node_type;
+	}
+	public void setNode_type(String node_type) {
+		this.node_type = node_type;
+	}
+	public String getNode_location() {
+		return node_location;
+	}
+	public void setNode_location(String node_location) {
+		this.node_location = node_location;
+	}
+	public String getNode_comments() {
+		return node_comments;
+	}
+	public void setNode_comments(String node_comments) {
+		this.node_comments = node_comments;
+	}
+	public String getDistance() {
+		return distance;
+	}
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+	public String getRssi() {
+		return rssi;
+	}
+	public void setRssi(String rssi) {
+		this.rssi = rssi;
+	}
+	public String getLsnr() {
+		return lsnr;
+	}
+	public void setLsnr(String lsnr) {
+		this.lsnr = lsnr;
+	}
+	public String getDatr() {
+		return datr;
+	}
+	public void setDatr(String datr) {
+		this.datr = datr;
+	}
+	public String getCounter_up() {
+		return counter_up;
+	}
+	public void setCounter_up(String counter_up) {
+		this.counter_up = counter_up;
+	}
+	public String getGw_addr() {
+		return gw_addr;
+	}
+	public void setGw_addr(String gw_addr) {
+		this.gw_addr = gw_addr;
+	}
+	public String getDev_addr() {
+		return dev_addr;
+	}
+	public void setDev_addr(String dev_addr) {
+		this.dev_addr = dev_addr;
+	}
+	public String getPort() {
+		return port;
+	}
+	public void setPort(String port) {
+		this.port = port;
+	}
+	public String getStat() {
+		return stat;
+	}
+	public void setStat(String stat) {
+		this.stat = stat;
+	}
+	public String getGw_band() {
+		return gw_band;
+	}
+	public void setGw_band(String gw_band) {
+		this.gw_band = gw_band;
+	}
+	public String getServer_time() {
+		return server_time;
+	}
+	public void setServer_time(String server_time) {
+		this.server_time = server_time;
+	}
+	public String getModu() {
+		return modu;
+	}
+	public void setModu(String modu) {
+		this.modu = modu;
+	}
+	public String getChan() {
+		return chan;
+	}
+	public void setChan(String chan) {
+		this.chan = chan;
+	}
+	public String getGateway_time() {
+		return gateway_time;
+	}
+	public void setGateway_time(String gateway_time) {
+		this.gateway_time = gateway_time;
+	}
+	public String getTmst() {
+		return tmst;
+	}
+	public void setTmst(String tmst) {
+		this.tmst = tmst;
+	}
+	public String getCodr() {
+		return codr;
+	}
+	public void setCodr(String codr) {
+		this.codr = codr;
+	}
+	public String getRfch() {
+		return rfch;
+	}
+	public void setRfch(String rfch) {
+		this.rfch = rfch;
+	}
+	public String getFreq() {
+		return freq;
+	}
+	public void setFreq(String freq) {
+		this.freq = freq;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getDev_eui() {
+		return dev_eui;
+	}
+	public void setDev_eui(String dev_eui) {
+		this.dev_eui = dev_eui;
+	}
+	public String getRx_time() {
+		return rx_time;
+	}
+	public void setRx_time(String rx_time) {
+		this.rx_time = rx_time;
+	}
+	public String getPayload() {
+		return payload;
+	}
+	public void setPayload(String payload) {
+		this.payload = payload;
+	}
+	public String getEndoffile() {
+		return endoffile;
+	}
+	public void setEndoffile(String endoffile) {
+		this.endoffile = endoffile;
+	}
+	String time;
+	String id;
+	String net_type;
 
+	
+	
+	String gw_alt;
+	String gw_ant_type;
+	String gw_location;
+	String gw_comments;
+	String node_lat;
+	String node_long;
+	String node_alt;
+	String node_type;
+	String node_location;
+	String node_comments;
+	String distance;
+	String rssi;
+	String lsnr;
+	String datr;
+	String counter_up;
+	String gw_addr;
+	String dev_addr;
+	String port;            
+	String stat;            
+	String gw_band;            
+	String server_time;          
+	String modu;            
+	String chan;            
+	String gateway_time;         
+	String tmst;            
+	String codr;            
+	String rfch;            
+	String freq;            
+	String size;            
+	String dev_eui;            
+	String rx_time;            
+	String payload;            
+	String endoffile;   
 
     
 }
--- src/api/RESTService.java
+++ src/api/RESTService.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,99 +16,231 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.json.JSONException;
 
-@Path ("/")
+@Path("/")
 public class RESTService {
 
-   private static String filePath = "//home//pi/app";
-   private static String fileName = "out.csv";
-   private static String secsData = "60";
+	// private static String filePath = "//home//pi/app";
+	private static String filePath = "c:\\";
+	private static String fileName = "out.csv";
+	private static String timeInMins = "9999";
 
-   @GET
-   @Path ("/get")
-   @Produces (MediaType.APPLICATION_JSON)
-   public List<Frame> getdata() throws JSONException {
+	@GET
+	@Path("/frames_secure")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Frame> getFramesSecure(@QueryParam("timeinmins") String timeInMins,
+			@QueryParam("spreadingfactor") String spreadingFactor, @QueryParam("key") String apiKey)
+			throws JSONException {
 
-      List<Frame> frames = null;
+		if (apiKey.equals("DFJJ-DFD-223-SD-SDSD")) {
+			return getFrames(timeInMins, spreadingFactor);
 
-      try {
-         frames = Files.readAllLines(Paths.get(filePath, fileName)).stream()
-               .map(s -> new Frame(s.split(",")[0], s.split(",")[1], s.split(",")[2], s.split(",")[3], s.split(",")[4],
-                     s.split(",")[5], s.split(",")[6], s.split(",")[7], s.split(",")[8]))
-               .filter(x -> inLastMin(x.getTime())).collect(Collectors.toList());
-      }
-      catch (IOException e) {
-         e.printStackTrace();
-      }
+		}
+		return null;
+	}
 
-      return frames;
-   }
+	@GET
+	@Path("/frames")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Frame> getFrames(@QueryParam("timeinmins") String timeInMins,
+			@QueryParam("spreadingfactor") String spreadingFactor) throws JSONException {
 
-   @POST
-   @Path ("/postform")
-   @Produces (MediaType.TEXT_HTML)
-   @Consumes (MediaType.APPLICATION_FORM_URLENCODED)
-   public String writeFormResult(@FormParam ("fpath") String fpath, @FormParam ("fname") String fname,
-         @FormParam ("nsecs") String nsecs) {
+		List<Frame> frames = null;
 
-      filePath = fpath.trim();
-      fileName = fname.trim();
-      secsData = nsecs.trim();
+		try {
 
-      String str = "<!DOCTYPE html>" + "<html>" + //
-            "<body>" + "values updated!" +
-            "</body>" + //
-            "</html>";
+			frames = Files.readAllLines(Paths.get(filePath, fileName)).stream()
+					.map(s -> new Frame(s.split(",")[0], s.split(",")[1], s.split(",")[2], s.split(",")[3],
+							s.split(",")[4], s.split(",")[5], s.split(",")[6], s.split(",")[7], s.split(",")[8],
+							s.split(",")[9], s.split(",")[10], s.split(",")[11], s.split(",")[12], s.split(",")[13],
+							s.split(",")[14], s.split(",")[15], s.split(",")[16], s.split(",")[17], s.split(",")[18],
+							s.split(",")[19], s.split(",")[20], s.split(",")[21], s.split(",")[22], s.split(",")[23],
+							s.split(",")[24], s.split(",")[25], s.split(",")[26], s.split(",")[27], s.split(",")[28],
+							s.split(",")[29], s.split(",")[30], s.split(",")[31], s.split(",")[32], s.split(",")[33],
+							s.split(",")[34], s.split(",")[35], s.split(",")[36], s.split(",")[37]))
+					.filter(x -> filterData(x.getTime(), timeInMins, x.getDatr(), spreadingFactor))
+					.collect(Collectors.toList());
 
-      return str;
-   }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-   @GET
-   @Path ("/config")
-   @Produces (MediaType.TEXT_HTML)
-   public String writeForm() {
+		return frames;
+	}
 
-      String str = "<!DOCTYPE html>" + "<html>" + //
-            "<body>" + //
-            "<form action=\"postform\" method=\"post\" >" + //
-            " Path:<br>" + //
-            "  <input type=\"text\" name=\"fpath\" value=\"" + filePath + " \">" + //
-            "  <br>" + //
-            " Name:<br>" + //
-            "  <input type=\"text\" name=\"fname\" value=\"" + fileName + "\">" + //
-            "  <br>" + //
-            " Number of Secs:<br>" + //
-            "  <input type=\"text\" name=\"nsecs\" value=\"" + secsData + "\">" + //
-            "  <br><br>" + //
-            "  <input type=\"submit\" value=\"Submit\">" + //
-            "</form> " + //
-            "<p>click to update</p>" + //
-            "</body>" + //
-            "</html>";
+	/**
+	 * @param aDate
+	 * @param timeInMins
+	 * @return
+	 * 
+	 * 		Filter the Lora data by time
+	 */
+	private boolean filterData(String aDate, String timeInMins, String aDatr, String spreadingFactor) {
 
-      return str;
-   }
+		SimpleDateFormat formatnow = new SimpleDateFormat("MMM dd yyyy HH:mm:ss");
+		Date date1 = null;
+		try {
+			date1 = formatnow.parse(aDate.substring(4, aDate.length() - 15));
+		} catch (Exception e) {
+			return false;
+		}
 
-   private boolean inLastMin(String aDate) {
+		// filter on time
+		if ((System.currentTimeMillis() - date1.getTime()) < (Integer.parseInt(timeInMins) * 1000 * 60)) {
 
-      SimpleDateFormat formatnow = new SimpleDateFormat("MMM dd yyyy HH:mm:ss");
-      Date date1 = null;
-      try {
-         date1 = formatnow.parse(aDate.substring(4, aDate.length() - 15));
-      }
-      catch (Exception e) {
-         return false;
-      }
+			// filter on spreading factor
+			if (spreadingFactor.equals("0") || aDatr.equals("SF" + spreadingFactor + "BW125")) {
+				return true;
+			}
 
-      if((System.currentTimeMillis() - date1.getTime()) < ( Integer.parseInt(secsData) * 1000)) {
-         return true;
-      }
+		}
 
-      return false;
+		// set to true for testing
+		return false;
 
-   }
+	}
+
+	@GET
+	@Path("/map")
+	@Produces(MediaType.TEXT_HTML)
+	public String getMap(@QueryParam("timeinmins") String timeInMins,
+			@QueryParam("spreadingfactor") String spreadingFactor) throws JSONException {
+
+		List<Frame> frames = getFrames(timeInMins, spreadingFactor);
+
+		String str = "<!DOCTYPE html>" + //
+				"<html>" + //
+				"  <head>" + //
+				"    <title>Lora Coverage Map</title>" + //
+				"    <meta name=\"viewport\" content=\"initial-scale=1.0\">" + //
+				"    <meta charset=\"utf-8\">" + //
+				"    <style>" + //
+				"      /* Always set the map height explicitly to define the size of the div" + //
+				"       * element that contains the map. */" + //
+				"      #map {" + //
+				"        height: 100%;" + //
+				"      }" + //
+				"      /* Optional: Makes the sample page fill the window. */" + //
+				"      html, body {" + //
+				"        height: 100%;" + //
+				"        margin: 0;" + //
+				"        padding: 0;" + //
+				"      }" + //
+				"    </style>" + //
+				"  </head>" + //
+				"  <body>" + //
+				"    <div id=\"map\"></div>" + //
+				"    <script>" + //
+				"      var map;" + //
+				"      function initMap() {" + //
+				"        map = new google.maps.Map(document.getElementById('map'), {" + //
+				"          center: {lat: 51.73113, lng: -0.36209}," + //
+				"          zoom: 15" + //
+				"        });" + //
+				"        " + //
+				"        	" + //
+				"        	";
+
+		ArrayList<String> data = new ArrayList<String>();
+		for (int i = 0; i < frames.size(); i++) {
+
+			// choose colour depending on spreading factor
+
+			String strokeColor = "";
+
+			if (frames.get(i).getDatr().equals("SF7BW125")) {
+				strokeColor = "#7DFF33";
+			}
+
+			if (frames.get(i).getDatr().equals("SF8BW125")) {
+				strokeColor = "#33FF38";
+			}
+
+			if (frames.get(i).getDatr().equals("SF9BW125")) {
+				strokeColor = "#33FF78";
+			}
+
+			if (frames.get(i).getDatr().equals("SF10BW125")) {
+				strokeColor = "#F8FF33";
+			}
+
+			if (frames.get(i).getDatr().equals("SF11BW125")) {
+				strokeColor = "#FF9233";
+			}
+
+			if (frames.get(i).getDatr().equals("SF12BW125")) {
+				strokeColor = "#FF4133";
+			}
+
+			str += "          var flightPath = new google.maps.Polyline({" + //
+					"              path: [{lat: " + frames.get(i).getGw_lat() + ", lng: " + frames.get(i).getGw_lon()
+					+ "},{lat: " + frames.get(i).getNode_lat() + ", lng:" + frames.get(i).getNode_long() + "}]," + //
+					"              geodesic: true," + //
+					"              map: map," + //
+					"              strokeColor: ' " + strokeColor + "'," + //
+					"              strokeOpacity: 1.0," + //
+					"              strokeWeight: 1" + //
+					"            });";
+
+		}
+
+		str += "      }" + //
+				"    </script>" + //
+				"    <script src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyAPA4oCk_64JNHmu64WTmLqq8e1--21wYU&callback=initMap\""
+				+ //
+				"    async defer></script>" + //
+				"  </body>" + //
+				"</html>";
+
+		return str;
+	}
+
+	@GET
+	@Path("/config")
+	@Produces(MediaType.TEXT_HTML)
+	public String writeForm() {
+
+		String str = "<!DOCTYPE html>" + "<html>" + //
+				"<body>" + //
+				"<form action=\"postform\" method=\"post\" >" + //
+				" Path:<br>" + //
+				"  <input type=\"text\" name=\"fpath\" value=\"" + filePath + " \">" + //
+				"  <br>" + //
+				" Name:<br>" + //
+				"  <input type=\"text\" name=\"fname\" value=\"" + fileName + "\">" + //
+				"  <br>" + //
+				" Number of Mins:<br>" + //
+				"  <input type=\"text\" name=\"nsecs\" value=\"" + timeInMins + "\">" + //
+				"  <br><br>" + //
+				"  <input type=\"submit\" value=\"Submit\">" + //
+				"</form> " + //
+				"<p>Click submit to update</p>" + //
+				"</body>" + //
+				"</html>";
+
+		return str;
+	}
+
+	@POST
+	@Path("/postform")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String writeFormResult(@FormParam("fpath") String fpath, @FormParam("fname") String fname,
+			@FormParam("nsecs") String nsecs) {
+
+		filePath = fpath.trim();
+		fileName = fname.trim();
+		timeInMins = nsecs.trim();
+
+		String str = "<!DOCTYPE html>" + "<html>" + //
+				"<body>" + "values updated!" + "</body>" + //
+				"</html>";
+
+		return str;
+	}
 
 }
